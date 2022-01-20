326856d1132f199de504eafabf10bfd61332cdfa
aux-Issa <issa1116184028@gmail.com>
2022-01-20 23:29:07 +0900

    integrate all of alogos

--- itoi/PIV_assignment/integration_algo.m
+++ itoi/PIV_assignment/integration_algo.m
@@ -41,29 +41,28 @@ for i = 1:num_data
     velocity_data{1,i}(:,1) = rmmissing(raw_velocity_data{1,i}(:,1));
     velocity_data{1,i}(:,2) = rmmissing(raw_velocity_data{1,i}(:,2));
 end
-disp(velocity_data)
+velocity_data
+disp('velocity_data') 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% 速度uの時空間平均
+% 速度uの時間平均
 time_u_sum = 0;
 for i=1:num_data
-%     time_u_sum = time_u_sum+fixed_velocity_data{1,i}(:,1);
-time_u_sum = time_u_sum+velocity_data{1,i}(:,1);
+    time_u_sum = time_u_sum+velocity_data{1,i}(:,1);
 end    
-time_u_mean=time_u_sum/num_data;
+raw_time_u_mean = time_u_sum / num_data;
 
-exact_time_u_mean = zeros(n,m);
+% 速度uの時間平均を座標系に整形
+time_u_mean = zeros(n,m);
 initialIndex = 1;
 for i=1:n
- exact_time_u_mean(i , :) = time_u_mean(initialIndex : (initialIndex + m - 1) , 1);
+ time_u_mean(i , :) = raw_time_u_mean(initialIndex : (initialIndex + m - 1) , 1);
  initialIndex = initialIndex + m;
 end    
-% fixed_time_u_mean = reshape(time_u_mean,[m,n]);
-% fixed_coord_y = reshape(coord.data(:,2),[m,n]);
-% 
+
 u_time_space_sum = 0;
 for i=1:m
-    u_time_space_sum = u_time_space_sum + exact_time_u_mean(:,i);
+    u_time_space_sum = u_time_space_sum + time_u_mean(:,i);
 end 
 u_time_space_sum = u_time_space_sum/m;
 
@@ -126,7 +125,7 @@ end
 u_dash = zeros(m*n,num_data);
 for i=1:num_data
     for v= 1:m*n
-        u_dash(v,i) = u_dash(v,i) + velocity_data{1,i}(v,1)-time_u_mean(v,1);
+        u_dash(v,i) = u_dash(v,i) + velocity_data{1,i}(v,1) - raw_time_u_mean(v,1);
     end
 end 
 v_dash = zeros(m*n,num_data);
