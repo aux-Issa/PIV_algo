2ffa9d330874237b97be646652c32848b3e686fa
aux-Issa <issa1116184028@gmail.com>
2022-01-22 16:24:19 +0900

    a liitel fix

--- hara_Ave_2.m
+++ hara_Ave_2.m
@@ -1,4 +1,4 @@
-ew%%
+%%
 close all;                          % close all figures
 clear;                              % clear all variables
 clc;                                % clear the command terminal
@@ -6,45 +6,53 @@ tic;                                % start of measuring the elapsed time
 
 %%
 % parameter part
-numfiles = 500;                     % total number of file                      
-m        = 128;                     % number of y direction
-n        = 253;                     % number of x direction
-ub       = 0.67;                    % bulk velocity [m/s]
-tauw     = 1.563703143;             % wall shear stress [N/m^2]
+numfiles = 100;                     % total number of file                      
+m        = 63;                      % number of x direction
+n        = 53;                      % number of y direction
+ub       = 0.335;                   % bulk velocity [m/s]
+tauw     = 0.605;                   % wall shear stress [N/m^2]
 ro       = 997.05;                  % density [kg/m^3]
 ut       = sqrt(tauw/ro);           % friction velocity [m/s] 
 vis      = 0.000000893;             % kinetic viscosity [m^2/s]
-h        = 0.01;                    % chanel half height [m]
-um       = zeros(m,n,2);            % matlix for velocity 
-uu       = zeros(m,n,3);            % matlix for velocity fluctuation
+h        = 0.02;                    % chanel half height [m]
+um       = zeros(n,m,2);            % matlix for velocity 
+uu       = zeros(n,m,3);            % matlix for velocity fluctuation
 
+%  input vec. 
+%                  wall
+%                   m
+%              |---------|
+%             n|  Flow�� |
+%           ��y|---------|
+%             o �� x  
+% It is noted that the normal direction is reversed in the calculation owting to the wall location.
 %%
 % reading part_coordinate_data
-loadname   = ('COORD.dat');                                        % inputting file name
+loadname   = ('/Volumes/HDCZ-UT/itoi_PIV/water/water_test.6uvaasgh/water_experiment.6vari2x3/coordinate_32px_v2.6vas9dlb.000000.dat');                                        % inputting file name
 coord      = importdata(loadname);                                 % making structure of above
-xy         = coord.data;                                           % separating text and data in structure
-xy         = xy-xy(1,:);                                           % set x1 to zero
-x(:,:,1)   = col2im(xy(:,1),[m n],[m n],'distinct');               % x data [mm]
-x(:,:,2)   = col2im(xy(:,2),[m n],[m n],'distinct');               % y data [mm]
+x(:,:,1)   = rot90(reshape(coord.data(:,1),[m n]));                % x data [mm]
+x(:,:,2)   = rot90(reshape(coord.data(:,2),[m n]));                % y data [mm]
+x(:,:,2)   = flip(x(:,:,2),1);                                     % y data [mm] with flip
 x          = x/1000;                                               % xy data [mm]��[m]
+
 
 %%
 % reading part_velocity_data_average
 for k =0:numfiles-1
-	if k<10                                                         % changing file name to read data
-	 file_name = sprintf('water.somehow1.00000%d.dat',k);
+	if k<10                                                        % changing file name to read data
+	 file_name = sprintf('velocity_32px.6vas9dlb.00000%d.dat',k);
 	elseif k<100
-	 file_name = sprintf('water.somehow1.0000%d.dat',k);
+	 file_name = sprintf('velocity_32px.6vas9dlb.0000%d.dat',k);
 	elseif k<1000
-	 file_name = sprintf('water.somehow1.000%d.dat',k);
+	 file_name = sprintf('velocity_32px.6vas9dlb.000%d.dat',k);
 	elseif k<10000 
-	 file_name = sprintf('water.somehow1.00%d.dat',k);
+	 file_name = sprintf('velocity_32px.6vas9dlb.00%d.dat',k);
     end
 
-    myfilename = sprintf('DATA/%s',file_name);
+    myfilename = sprintf('/Volumes/HDCZ-UT/itoi_PIV/water/water_test.6uvaasgh/water_experiment.6vari2x3/velocity/%s',file_name);
     mydata     = importdata(myfilename);
-    u(:,:,1)   = col2im(mydata.data(:,1),[m n],[m n],'distinct');   % u
-    u(:,:,2)   = col2im(mydata.data(:,2),[m n],[m n],'distinct');   % v
+    u(:,:,1)   = rot90(reshape(mydata.data(:,1),[m n]));            % u
+    u(:,:,2)   = -rot90(reshape(mydata.data(:,2),[m n]));           % v
 
     um(:,:,1)  = um(:,:,1)+u(:,:,1)/double(numfiles);               % time ave. u
     um(:,:,2)  = um(:,:,2)+u(:,:,2)/double(numfiles);               % time ave. v
@@ -53,13 +61,13 @@ end
     ums(:,1)  = mean(um(:,:,1),2);                                  % emsemble ave. u
     ums(:,2)  = mean(um(:,:,2),2);                                  % emsemble ave. u
 
-%%
-% 2D-figure
+% %%
+% % 2D-figure
 figure;
 newpoints = m*3;
 [xq,yq] = meshgrid(...                                              % making fine mesh
-               linspace(x(1,1,1),x(1,n,1),newpoints ),...
-               linspace(x(1,1,2),x(m,1,2),newpoints )...
+               linspace(x(1,1,1),x(1,m,1),newpoints ),...
+               linspace(x(1,1,2),x(n,1,2),newpoints )...
              );
 Umxy = interp2(x(:,:,1),x(:,:,2),um(:,:,1),xq,yq,'spline');         % interpolation
 [cc,hc]=contourf(xq/h,yq/h,Umxy/ub,12);                             % making isoline
@@ -68,8 +76,8 @@ hc.TextStep = 0.4;                                                  % interval i
 hc.ShowText = 'off';                                                % isoline text off
 colormap('jet');                                                    % color type of 'jet'
 box on;                                                             % making flame of figure
-xlim([0 4]);                                                        % range of x
-ylim([0 2]);                                                        % range of y
+% xlim([0 4]);                                                       % range of x
+% ylim([0 0.5]);                                                     % range of y
 set( gca, 'FontName','Times','FontSize',18);                        % font and its size of axes 
 ax = gca;
 ax.TickLength = [0.02 0.1];                                         % scale size to inside from flame
@@ -86,21 +94,22 @@ c.Label.String = '$${\it \overline{u}}/{\it U_b}$$';                % label of c
 %c.Label.String = '$${\it \overline{u^+}}$$';                       % label of color bar
 c.Label.FontSize = 20;                                              % font size of label of color bar
 saveas(gcf,'Umxy','emf');                                           % output
-
-%%
+% 
+% %%
 % 1D-figure
 figure;
-p = plot(decimate(x(1:m,1,2),2)/h,decimate(ums(:,1),2)/ub,'ko');
+% p = plot(decimate(x(1:n,1,2),2)/h,decimate(ums(:,1),2)/ub,'ko');
+p = plot(x(1:n,1,2)/h,ums(:,1)/ub,'ko');
 p.LineWidth = 1.2;
 p.MarkerSize = 10;
 p.MarkerEdgeColor = 'black';
 p.MarkerFaceColor = [0.1 1 1];
 box on;
 %axis equal tight;                                                 
-xticks([0:0.5:2]);
+xticks([0:0.2:1]);
 yticks([0:0.2:1.4]);
-xlim([0 2]);
-ylim([0 1.4]);
+xlim([0 1]);
+ylim([0 1.3]);
 set( gca, 'FontName','Times','FontSize',18); 
 ax = gca;
 ax.TickLength = [0.02 0.1];
@@ -111,20 +120,20 @@ saveas(gcf,'Um-y','emf');
 %%
 % reading part_velocity_data_fluctuation
 for k =0:numfiles-1
-	if k<10
-	 file_name = sprintf('water.somehow1.00000%d.dat',k);
+	if k<10                                                         % changing file name to read data
+	 file_name = sprintf('B000%d.dat',k);
 	elseif k<100
-	 file_name = sprintf('water.somehow1.0000%d.dat',k);
+	 file_name = sprintf('B00%d.dat',k);
 	elseif k<1000
-	 file_name = sprintf('water.somehow1.000%d.dat',k);
+	 file_name = sprintf('B0%d.dat',k);
 	elseif k<10000 
-	 file_name = sprintf('water.somehow1.00%d.dat',k);
+	 file_name = sprintf('B%d.dat',k);
     end
 
     myfilename = sprintf('DATA/%s',file_name);
     mydata     = importdata(myfilename);
-    u(:,:,1)   = col2im(mydata.data(:,1),[m n],[m n],'distinct');
-    u(:,:,2)   = col2im(mydata.data(:,2),[m n],[m n],'distinct');
+    u(:,:,1)   = rot90(reshape(mydata.data(:,1),[m n]));            % u
+    u(:,:,2)   = -rot90(reshape(mydata.data(:,2),[m n]));           % v
 
     uf(:,:,1)  = u(:,:,1)-ums(:,1);
     uf(:,:,2)  = u(:,:,2)-ums(:,2);
@@ -139,76 +148,75 @@ end
     uus(:,1)      = sqrt(mean(uu(:,:,1),2));
     uus(:,2)      = sqrt(mean(uu(:,:,2),2));
     uus(:,3)      = mean(uu(:,:,3),2);
-    euv(1:m-1,1)  = uus(1:m-1,3)./diff(ums(:,1)).*diff(x(:,1,2));
-
-%%
+    euv(1:n-1,1)  = uus(1:n-1,3)./diff(ums(:,1)).*diff(x(:,1,2));
+% 
 % 1D-figure
 figure;
 subplot(2,2,1);
-p = plot(decimate(x(1:m,1,2),4)/h,decimate(uus(:,1),4)/ut,'ko');
+p = plot(x(1:n,1,2)/h,uus(:,1)/ut,'ko');
 p.LineWidth = 1.2;
 p.MarkerSize = 7;
 p.MarkerEdgeColor = 'black';
 p.MarkerFaceColor = [0.1 1 1];
 box on;
 %axis equal tight;                                                 
-xticks([0:0.5:2]);
+xticks([0:0.2:1]);
 yticks([0:0.5:2.5]);
-xlim([0 2]);
+xlim([0 1]);
 ylim([0 2.5]);
 set( gca, 'FontName','Times','FontSize',18); 
 ax = gca;
 ax.TickLength = [0.02 0.1];
 xlabel('{\it y}/{\it h}','FontSize',20,'Interpreter','latex');
 ylabel('$${\it u^\prime}_{{\rm rms}}^+$$','FontSize',20,'Interpreter','latex');
-
+% 
 subplot(2,2,2);
-p = plot(decimate(x(1:m,1,2),4)/h,decimate(uus(:,2),4)/ut,'k^');
+p = plot(x(1:n,1,2)/h,uus(:,2)/ut,'k^');
 p.LineWidth = 1.2;
 p.MarkerSize = 7;
 p.MarkerEdgeColor = 'black';
 p.MarkerFaceColor = [1 0.2 1];
 box on;
 %axis equal tight;                                                 
-xticks([0:0.5:2]);
+xticks([0:0.2:1]);
 yticks([0:0.5:1.5]);
-xlim([0 2]);
+xlim([0 1]);
 ylim([0 1.5]);
 set( gca, 'FontName','Times','FontSize',18); 
 ax = gca;
 ax.TickLength = [0.02 0.1];
 xlabel('{\it y}/{\it h}','FontSize',20,'Interpreter','latex');
 ylabel('$${\it v^\prime}_{{\rm rms}}^+$$','FontSize',20,'Interpreter','latex');
-
+% 
 subplot(2,2,3);
-p = plot(decimate(x(1:m,1,2),4)/h,decimate(uus(:,3),4)/(ut*ut),'k>');
+p = plot(x(1:n,1,2)/h,uus(:,3)/(ut*ut),'k>');
 p.LineWidth = 1.2;
 p.MarkerSize = 7;
 p.MarkerEdgeColor = 'black';
 p.MarkerFaceColor = [0.5 0.5 1];
 box on;
 %axis equal tight;                                                 
-xticks([0:0.5:2]);
+xticks([0:0.2:1]);
 yticks([-1:0.5:1.0]);
-xlim([0 2]);
-ylim([-1 1]);
+xlim([0 1]);
+ylim([0 1]);
 set( gca, 'FontName','Times','FontSize',18); 
 ax = gca;
 ax.TickLength = [0.02 0.1];
 xlabel('{\it y}/{\it h}','FontSize',20,'Interpreter','latex');
 ylabel('$$-\overline{{\it u^\prime}^+{\it v^\prime}^+}$$','FontSize',20,'Interpreter','latex');
-
+% 
 subplot(2,2,4);
-p = plot(decimate(x(1:m-1,1,2),2)/h,decimate(euv(:,1),2)/vis,'ko');
+p = plot(x(1:n-1,1,2)/h,euv(:,1)/vis,'ko');
 p.LineWidth = 1.2;
 p.MarkerSize = 7;
 p.MarkerEdgeColor = 'black';
 p.MarkerFaceColor = [0.1 0.1 1];
 box on;
 %axis equal tight;                                                 
-xticks([0:0.5:2]);
+xticks([0:0.2:1]);
 yticks([0:20:80]);
-xlim([0 2]);
+xlim([0 1]);
 ylim([0 80]);
 set( gca, 'FontName','Times','FontSize',18); 
 ax = gca;
@@ -216,6 +224,6 @@ ax.TickLength = [0.02 0.1];
 xlabel('{\it y}/{\it h}','FontSize',20,'Interpreter','latex');
 ylabel('$$\epsilon_{u^\prime v^\prime}^+$$','FontSize',20,'Interpreter','latex');
 saveas(gcf,'Fluc-y','emf');
-
-%%
+% 
+% %
     toc;
