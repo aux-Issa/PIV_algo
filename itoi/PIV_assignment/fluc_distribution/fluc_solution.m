59df28c8d0be6ef39d0e1e59c42efe2cfd53d7c7
aux-Issa <issa1116184028@gmail.com>
2022-02-10 17:55:48 +0900

    変動の分布図

--- itoi/PIV_assignment/fluc_distribution/fluc_solution.m
+++ itoi/PIV_assignment/fluc_distribution/fluc_solution.m
@@ -8,23 +8,23 @@ newpoints = m*3;
 %              );
 %  クエリ点で内挿
 U_dash= interp2(x(:,:,1),x(:,:,2),uf(:,:,1),xq,yq,'spline');         % interpolation
-[cc_solution_dash,hc_solution_dash]=contourf(xq*ut/vis,yq*ut/vis,U_dash/Ub,10);                   % making isoline
+[cc_solution_dash,hc_solution_dash]=contourf(xq*ut/vis,yq*ut/vis,U_dash/ub,20);                   % making isoline
 hc_solution_dash.TextStep = 0.4;                                                  % interval isoline
 hc_solution_dash.ShowText = 'off';                                                % isoline text off
 colormap('jet');                                                    % color type of 'jet'
 box on;                                                             % making flame of figure
-xlim([0 300]);                                                       % range of x
-ylim([0 100]);                                                     % range of y
+xlim([0 150]);                                                       % range of x
+ylim([0 60]);                                                     % range of y
 set( gca, 'FontName','Times','FontSize',18);                        % font and its size of axes 
 ax = gca;
 ax.TickLength = [0.02 0.1];                                         % scale size to inside from flame
-xlabel('{\it x}^+','FontSize',20,'Interpreter','latex');           % xlabel, its size, and type
-ylabel('{\it y}^+','FontSize',20,'Interpreter','latex');           % ylabel, its size, and type
+xlabel('${\it x^+}$','FontSize',20,'Interpreter','latex');           % xlabel, its size, and type
+ylabel('${\it y^+}$','FontSize',20,'Interpreter','latex');           % ylabel, its size, and type
 c = colorbar;                                                       % making color bar
-c.Limits = [0 10.0];                                                 % range of colorbar
+c.Limits = [-0.4 0.4];                                                 % range of colorbar
 c.FontSize = 18;                                                    % font size of scale of color bar
 c.TickLabelInterpreter = 'latex';                                   % font type of scale of color bar
 c.Label.Interpreter = 'latex';                                      % font type of label of color bar
-c.Label.String = '$${\it \u_dash}/{\it \overline{u^+}}$$';                % label of color bar
+c.Label.String = '$${\it u^\prime}/{\it U_b}$$';                % label of color bar
 c.Label.FontSize = 20;                                              % font size of label of color bar
-saveas(gcf,'U_dash','png');   
+saveas(gcf,'U_dash_solution','png');   
