2799591f1d7911a8173d78f9100359b11dedd7d7
aux-Issa <issa1116184028@gmail.com>
2022-02-14 20:40:10 +0900

    Restrssを無次元化

--- itoi/PIV_assignment/comparision_In_elastozone.m
+++ itoi/PIV_assignment/comparision_In_elastozone.m
@@ -4,6 +4,8 @@ water_v_RMS_plot_Yaxis = load('session_waterRMS', 'water_v_RMS_plot_Yaxis').wate
 solution_plot_Xaxis = load('session_solutionRMS', 'solution_plot_Xaxis').solution_plot_Xaxis;
 solution_u_RMS_plot_Yaxis = load('session_solutionRMS', 'solution_u_RMS_plot_Yaxis').solution_u_RMS_plot_Yaxis;
 solution_v_RMS_plot_Yaxis = load('session_solutionRMS', 'solution_v_RMS_plot_Yaxis').solution_v_RMS_plot_Yaxis;
+water_Normalized_Re_stress = load('water_Normalized_Re_stress', 'water_Normalized_Re_stress').water_Normalized_Re_stress;
+solution_Normalized_Re_stress = load('solution_Normalized_Re_stress', 'solution_Normalized_Re_stress').solution_Normalized_Re_stress;
 
 % p_u_water = plot(water_plot_Xaxis,water_u_RMS_plot_Yaxis,'b^');
 % p_u_water.LineWidth = 1.2;
@@ -21,17 +23,32 @@ solution_v_RMS_plot_Yaxis = load('session_solutionRMS', 'solution_v_RMS_plot_Yax
 % legend({'water','80ppm'},'Location','northeast')
 % hold off 
 
-p_v_water = plot(water_plot_Xaxis,water_v_RMS_plot_Yaxis,'b^');
+% p_v_water = plot(water_plot_Xaxis,water_v_RMS_plot_Yaxis,'b^');
+% p_v_water.LineWidth = 1.2;
+% p_v_water.MarkerSize = 7;
+% p_v_water.MarkerFaceColor = 'blue';
+% box on;
+% xlabel('$${\it y^+}$$','FontSize',20,'Interpreter','latex');
+% ylabel('$${\it v^\prime}_{{\rm rms}}^+$$','FontSize',20,'Interpreter','latex');
+% xlim([0 60]); 
+% hold on
+% p_v_solution = plot(solution_plot_Xaxis,solution_v_RMS_plot_Yaxis,'r^');
+% p_v_solution.MarkerEdgeColor = 'red';
+% p_v_solution.MarkerFaceColor = 'red';
+% legend({'water','80ppm'},'Location','northeast')
+% hold off 
+
+% 無次元化されたレイノルズ剪断応力の粘弾性底層内の比較
+p_v_water = plot(water_plot_Xaxis,water_Normalized_Re_stress,'b^');
 p_v_water.LineWidth = 1.2;
 p_v_water.MarkerSize = 7;
 p_v_water.MarkerFaceColor = 'blue';
 box on;
 xlabel('$${\it y^+}$$','FontSize',20,'Interpreter','latex');
-ylabel('$${\it v^\prime}_{{\rm rms}}^+$$','FontSize',20,'Interpreter','latex');
+ylabel('$$-\overline{{\it u^\prime}^+{\it v^\prime}^+}$$','FontSize',20,'Interpreter','latex');
 xlim([0 60]); 
 hold on
-p_v_solution = plot(solution_plot_Xaxis,solution_v_RMS_plot_Yaxis,'r^');
+p_v_solution = plot(solution_plot_Xaxis,solution_Normalized_Re_stress,'r^');
 p_v_solution.MarkerEdgeColor = 'red';
-p_v_solution.MarkerFaceColor = 'red';
 legend({'water','80ppm'},'Location','northeast')
 hold off 
