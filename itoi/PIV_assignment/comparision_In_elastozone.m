74a3c1b606d42eb53287f3251e85f63b4eb880c2
aux-Issa <issa1116184028@gmail.com>
2022-02-17 18:17:52 +0900

    ディレクトリを整理

--- itoi/PIV_assignment/comparision_In_elastozone.m
+++ itoi/PIV_assignment/comparision_In_elastozone.m
@@ -13,24 +13,31 @@ u_mean_DNS = DNSdata(:,3);
 u_RMS_DNS = DNSdata(:,4).^(1/2);
 v_RMS_DNS = DNSdata(:,5).^(1/2);
 
-% p_u_water = plot(water_plot_Xaxis,water_u_RMS_plot_Yaxis,'b^');
-% p_u_water.LineWidth = 1.2;
-% p_u_water.MarkerSize = 7;
-% % p_u_water.MarkerEdgeColor = 'blue';
-% p_u_water.MarkerFaceColor = 'blue';
-% box on;
-% xlabel('$${\it y^+}$$','FontSize',30,'Interpreter','latex');
-% ylabel('$${\it u^\prime}_{{\rm rms}}^+$$','FontSize',30,'Interpreter','latex');
-% xlim([0 60]); 
-% ylim([0 5]);
-% hold on
-% p_u_solution = plot(solution_plot_Xaxis,solution_u_RMS_plot_Yaxis,'ro');
-% p_u_solution.MarkerEdgeColor = 'red';
-% p_u_solution.MarkerFaceColor = 'red';
-% p_u_DNS = plot(y_cross_DNS,u_RMS_DNS,'k-');
+% DNS_lowRe_file_name = sprintf('/Users/issa/Desktop/DNS_Re.csv');
+% DNS_lowRe_data = csvread(DNS_lowRe_file_name, 1, 0);
+% y_cross_lowRe_DNS = DNS_lowRe_data(:,2);
+% u_mean_lowRe_DNS = DNS_lowRe_data(:,3);
+% u_RMS_lowRe_DNS = DNS_lowRe_data(:,4).^(1/2);
+% v_RMS_lowRe_DNS = DNS_lowRe_data(:,5).^(1/2);
 
-% legend({'water','80ppm','DNS'},'Location','northeast')
-% hold off 
+p_u_water = plot(water_plot_Xaxis,water_u_RMS_plot_Yaxis,'b^');
+p_u_water.LineWidth = 1.2;
+p_u_water.MarkerSize = 7;
+p_u_water.MarkerFaceColor = 'blue';
+box on;
+xlabel('$${\it y^+}$$','FontSize',30,'Interpreter','latex');
+ylabel('$${\it u^\prime}_{{\rm rms}}^+$$','FontSize',30,'Interpreter','latex');
+xlim([0 60]); 
+ylim([0 5]);
+hold on
+p_u_solution = plot(solution_plot_Xaxis,solution_u_RMS_plot_Yaxis,'ro');
+p_u_solution.MarkerEdgeColor = 'red';
+p_u_solution.MarkerFaceColor = 'red';
+p_u_DNS = plot(y_cross_DNS,u_RMS_DNS,'k-');
+% p_u_lowRe_DNS = plot(y_cross_lowRe_DNS,u_RMS_lowRe_DNS,'k^');
+
+legend({'water','80ppm','DNS'},'Location','northeast')
+hold off 
 
 % p_v_water = plot(water_plot_Xaxis,water_v_RMS_plot_Yaxis,'b^');
 % p_v_water.LineWidth = 1.2;
@@ -45,6 +52,7 @@ v_RMS_DNS = DNSdata(:,5).^(1/2);
 % p_v_solution = plot(solution_plot_Xaxis,solution_v_RMS_plot_Yaxis,'ro');
 % p_v_solution.MarkerEdgeColor = 'red';
 % p_v_solution.MarkerFaceColor = 'red';
+% p_v_DNS = plot(y_cross_DNS,v_RMS_DNS,'k-');
 % legend({'water','80ppm'},'Location','northeast')
 % hold off 
 
