c47536565acd145206920869f8eb858863ec35a1
aux-Issa <issa1116184028@gmail.com>
2022-02-17 10:53:24 +0900

    軸ラベルの大きさ調整

--- itoi/PIV_assignment/comparision_In_elastozone.m
+++ itoi/PIV_assignment/comparision_In_elastozone.m
@@ -13,46 +13,48 @@ solution_Normalized_Re_stress = load('solution_Normalized_Re_stress', 'solution_
 % % p_u_water.MarkerEdgeColor = 'blue';
 % p_u_water.MarkerFaceColor = 'blue';
 % box on;
-% xlabel('$${\it y^+}$$','FontSize',20,'Interpreter','latex');
-% ylabel('$${\it u^\prime}_{{\rm rms}}^+$$','FontSize',20,'Interpreter','latex');
+% xlabel('$${\it y^+}$$','FontSize',30,'Interpreter','latex');
+% ylabel('$${\it u^\prime}_{{\rm rms}}^+$$','FontSize',30,'Interpreter','latex');
 % xlim([0 60]); 
+% ylim([0 5]);
 % hold on
-% p_u_solution = plot(solution_plot_Xaxis,solution_u_RMS_plot_Yaxis,'r^');
+% p_u_solution = plot(solution_plot_Xaxis,solution_u_RMS_plot_Yaxis,'ro');
 % p_u_solution.MarkerEdgeColor = 'red';
 % p_u_solution.MarkerFaceColor = 'red';
 % legend({'water','80ppm'},'Location','northeast')
 % hold off 
 
-% p_v_water = plot(water_plot_Xaxis,water_v_RMS_plot_Yaxis,'b^');
-% p_v_water.LineWidth = 1.2;
-% p_v_water.MarkerSize = 7;
-% p_v_water.MarkerFaceColor = 'blue';
-% box on;
-% xlabel('$${\it y^+}$$','FontSize',20,'Interpreter','latex');
-% ylabel('$${\it v^\prime}_{{\rm rms}}^+$$','FontSize',20,'Interpreter','latex');
-% xlim([0 60]); 
-% hold on
-% p_v_solution = plot(solution_plot_Xaxis,solution_v_RMS_plot_Yaxis,'r^');
-% p_v_solution.MarkerEdgeColor = 'red';
-% p_v_solution.MarkerFaceColor = 'red';
-% legend({'water','80ppm'},'Location','northeast')
-% hold off 
-
-% 無次元化されたレイノルズ剪断応力の粘弾性底層内の比較
-p_v_water = plot(water_plot_Xaxis,water_Normalized_Re_stress,'b^');
+p_v_water = plot(water_plot_Xaxis,water_v_RMS_plot_Yaxis,'b^');
 p_v_water.LineWidth = 1.2;
 p_v_water.MarkerSize = 7;
-p_v_water.MarkerEdgeColor = 'blue';
 p_v_water.MarkerFaceColor = 'blue';
 box on;
-xlabel('$${\it y^+}$$','FontSize',20,'Interpreter','latex');
-ylabel('$$-\overline{{\it u^\prime}^+{\it v^\prime}^+}$$','FontSize',20,'Interpreter','latex');
+xlabel('$${\it y^+}$$','FontSize',30,'Interpreter','latex');
+ylabel('$${\it v^\prime}_{{\rm rms}}^+$$','FontSize',30,'Interpreter','latex');
 xlim([0 60]); 
-ylim([-0.6 1]); 
-yticks([-1:0.2:1.0]);
+ylim([0 5]);
 hold on
-p_v_solution = plot(solution_plot_Xaxis,solution_Normalized_Re_stress,'r^');
+p_v_solution = plot(solution_plot_Xaxis,solution_v_RMS_plot_Yaxis,'ro');
 p_v_solution.MarkerEdgeColor = 'red';
 p_v_solution.MarkerFaceColor = 'red';
 legend({'water','80ppm'},'Location','northeast')
 hold off 
+
+% 無次元化されたレイノルズ剪断応力の粘弾性底層内の比較
+% p_v_water = plot(water_plot_Xaxis,water_Normalized_Re_stress,'b^');
+% p_v_water.LineWidth = 1.2;
+% p_v_water.MarkerSize = 7;
+% p_v_water.MarkerEdgeColor = 'blue';
+% p_v_water.MarkerFaceColor = 'blue';
+% box on;
+% xlabel('$${\it y^+}$$','FontSize',20,'Interpreter','latex');
+% ylabel('$$-\overline{{\it u^\prime}^+{\it v^\prime}^+}$$','FontSize',20,'Interpreter','latex');
+% xlim([0 60]); 
+% ylim([-0.6 1]); 
+% yticks([-1:0.2:1.0]);
+% hold on
+% p_v_solution = plot(solution_plot_Xaxis,solution_Normalized_Re_stress,'r^');
+% p_v_solution.MarkerEdgeColor = 'red';
+% p_v_solution.MarkerFaceColor = 'red';
+% legend({'water','80ppm'},'Location','northeast')
+% hold off 
