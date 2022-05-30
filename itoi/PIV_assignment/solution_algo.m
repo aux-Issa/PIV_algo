667fba07adb29f3a607f6b6b0c32a8a0c87a8437
aux-Issa <issa1116184028@gmail.com>
2022-05-30 16:24:40 +0900

    リファクタ

--- itoi/PIV_assignment/solution_algo.m
+++ itoi/PIV_assignment/solution_algo.m
@@ -196,34 +196,7 @@ for k =0:numfiles-1
         c.Label.FontSize = 20;                                              % font size of label of color bar
         fig_name = sprintf('U_dash_solution%d',k);
         saveas(gcf,fig_name ,'png'); 
-        
     end 
-    % % if k < 15
-    % if k == 6 || k == 8|| k == 14
-    %     [cc_solution_dash,hc_solution_dash]=contourf(xq*ut/vis,yq*ut/vis,V_dash/ut,8);                   % making isoline
-    %     hc_solution_dash.TextStep = 0.4;                                                  % interval isoline
-    %     hc_solution_dash.ShowText = 'off';                                                % isoline text off
-    %     colormap('jet');                                                    % color type of 'jet'
-    %     box on;                                                             % making flame of figure
-    %     xlim([0 150]);                                                       % range of x
-    %     ylim([0 60]);                                                     % range of y
-    %     set( gca, 'FontName','Times','FontSize',18);                        % font and its size of axes 
-    %     ax = gca;
-    %     ax.TickLength = [0.02 0.1];                                         % scale size to inside from flame
-    %     xlabel('${\it x^+}$','FontSize',20,'Interpreter','latex');           % xlabel, its size, and type
-    %     ylabel('${\it y^+}$','FontSize',20,'Interpreter','latex');           % ylabel, its size, and type
-    %     c = colorbar;                                                       % making color bar
-    %     % c.Limits = [-0.4 0.4];                                                 % range of colorbar
-    %     c.Limits = [-3 3];
-    %     c.FontSize = 18;                                                    % font size of scale of color bar
-    %     c.TickLabelInterpreter = 'latex';                                   % font type of scale of color bar
-    %     c.Label.Interpreter = 'latex';                                      % font type of label of color bar
-    %     % c.Label.String = '$${\it v^\prime}/{\it U_b}$$';                % label of color bar
-    %     c.Label.String = '$${\it u^\prime}^+$$';                             % label of color bar
-    %     c.Label.FontSize = 20;                                              % font size of label of color bar
-    %     fig_name = sprintf('V_dash_solution%d',k);
-    %     saveas(gcf,fig_name ,'png'); 
-    % end  
 
     % レイノルズ剪断応力の瞬時場
     % if k < 15
