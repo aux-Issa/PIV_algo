bb4d4cb139ec84e86ae3c4462902bc463062a0f2
aux-Issa <issa1116184028@gmail.com>
2022-05-31 18:33:51 +0900

    fix the number of figure

--- itoi/PIV_assignment/comparision_In_elastozone.m
+++ itoi/PIV_assignment/comparision_In_elastozone.m
@@ -12,7 +12,7 @@ y_cross_DNS = DNSdata(:,2);
 u_mean_DNS = DNSdata(:,3);
 u_RMS_DNS = DNSdata(:,4).^(1/2);
 v_RMS_DNS = DNSdata(:,5).^(1/2);
-
+f = 1;
 % DNS_lowRe_file_name = sprintf('/Users/issa/Desktop/DNS_Re.csv');
 % DNS_lowRe_data = csvread(DNS_lowRe_file_name);
 % y_cross_lowRe_DNS = DNS_lowRe_data(:,2);
@@ -20,6 +20,8 @@ v_RMS_DNS = DNSdata(:,5).^(1/2);
 % u_RMS_lowRe_DNS = DNS_lowRe_data(:,4).^(1/2);
 % v_RMS_lowRe_DNS = DNS_lowRe_data(:,5).^(1/2);
 
+%% Uのrms
+figure(f)
 p_u_water = plot(water_plot_Xaxis,water_u_RMS_plot_Yaxis,'b^');
 p_u_water.LineWidth = 1.2;
 p_u_water.MarkerSize = 7;
@@ -38,39 +40,45 @@ p_u_DNS = plot(y_cross_DNS,u_RMS_DNS,'k-');
 
 legend({'water','80ppm','DNS'},'Location','northeast')
 hold off 
+f = f + 1;
 
-% p_v_water = plot(water_plot_Xaxis,water_v_RMS_plot_Yaxis,'b^');
-% p_v_water.LineWidth = 1.2;
-% p_v_water.MarkerSize = 7;
-% p_v_water.MarkerFaceColor = 'blue';
-% box on;
-% xlabel('$${\it y^+}$$','FontSize',30,'Interpreter','latex');
-% ylabel('$${\it v^\prime}_{{\rm rms}}^+$$','FontSize',30,'Interpreter','latex');
-% xlim([0 60]); 
-% ylim([0 5]);
-% hold on
-% p_v_solution = plot(solution_plot_Xaxis,solution_v_RMS_plot_Yaxis,'ro');
-% p_v_solution.MarkerEdgeColor = 'red';
-% p_v_solution.MarkerFaceColor = 'red';
-% p_v_DNS = plot(y_cross_DNS,v_RMS_DNS,'k-');
-% legend({'water','80ppm'},'Location','northeast')
-% hold off 
+%% Vのrms
+figure(f)
+p_v_water = plot(water_plot_Xaxis,water_v_RMS_plot_Yaxis,'b^');
+p_v_water.LineWidth = 1.2;
+p_v_water.MarkerSize = 7;
+p_v_water.MarkerFaceColor = 'blue';
+box on;
+xlabel('$${\it y^+}$$','FontSize',30,'Interpreter','latex');
+ylabel('$${\it v^\prime}_{{\rm rms}}^+$$','FontSize',30,'Interpreter','latex');
+xlim([0 60]); 
+ylim([0 5]);
+hold on
+p_v_solution = plot(solution_plot_Xaxis,solution_v_RMS_plot_Yaxis,'ro');
+p_v_solution.MarkerEdgeColor = 'red';
+p_v_solution.MarkerFaceColor = 'red';
+p_v_DNS = plot(y_cross_DNS,v_RMS_DNS,'k-');
+legend({'water','80ppm'},'Location','northeast')
+hold off 
+f = f + 1;
 
 % 無次元化されたレイノルズ剪断応力の粘弾性底層内の比較
-% p_v_water = plot(water_plot_Xaxis,water_Normalized_Re_stress,'b^');
-% p_v_water.LineWidth = 1.2;
-% p_v_water.MarkerSize = 7;
-% p_v_water.MarkerEdgeColor = 'blue';
-% p_v_water.MarkerFaceColor = 'blue';
-% box on;
-% xlabel('$${\it y^+}$$','FontSize',20,'Interpreter','latex');
-% ylabel('$$-\overline{{\it u^\prime}^+{\it v^\prime}^+}$$','FontSize',20,'Interpreter','latex');
-% xlim([0 60]); 
-% ylim([-0.6 1]); 
-% yticks([-1:0.2:1.0]);
-% hold on
-% p_v_solution = plot(solution_plot_Xaxis,solution_Normalized_Re_stress,'r^');
-% p_v_solution.MarkerEdgeColor = 'red';
-% p_v_solution.MarkerFaceColor = 'red';
-% legend({'water','80ppm'},'Location','northeast')
-% hold off 
+figure(f)
+p_v_water = plot(water_plot_Xaxis,water_Normalized_Re_stress,'b^');
+p_v_water.LineWidth = 1.2;
+p_v_water.MarkerSize = 7;
+p_v_water.MarkerEdgeColor = 'blue';
+p_v_water.MarkerFaceColor = 'blue';
+box on;
+xlabel('$${\it y^+}$$','FontSize',20,'Interpreter','latex');
+ylabel('$$-\overline{{\it u^\prime}^+{\it v^\prime}^+}$$','FontSize',20,'Interpreter','latex');
+xlim([0 60]); 
+ylim([-0.6 1]); 
+yticks([-1:0.2:1.0]);
+hold on
+p_v_solution = plot(solution_plot_Xaxis,solution_Normalized_Re_stress,'ro');
+p_v_solution.MarkerEdgeColor = 'red';
+p_v_solution.MarkerFaceColor = 'red';
+legend({'water','80ppm'},'Location','northeast')
+hold off 
+toc;
